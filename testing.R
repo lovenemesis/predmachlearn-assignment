e18eb02f83d10fa9a9a22bfd7b01cc223d28db90
Tommy He <tommy.he@linux.com>
2015-01-25 14:04:15 +0800

    Updated into final version

--- testing.R
+++ testing.R
@@ -4,3 +4,6 @@ if(file.access("pml-testing.csv", mode = 4) == -1){
     download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv", "pml-testing.csv", method="curl")
     }
 pml_testing <- read.csv("pml-testing.csv")
+pml_testing <- pml_testing[,-nnv]
+pml_testing <- pml_testing[,-nsv]
+pml_testing <- pml_testing[,-(1:6)]
